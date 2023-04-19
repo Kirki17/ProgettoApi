6f178f6a16f7a95453264ea3fd4fa97559b1e823
lucrezia.kircanski <lucrezia.kircanski@marconirovereto.it>
2023-04-19 16:11:40 +0200

    aggiunta bottone precedente e successivo

--- razzeCanine/src/app/dati/dati.component.ts
+++ razzeCanine/src/app/dati/dati.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ignoreElements } from 'rxjs';
 import { LetturaService } from '../common/lettura.service';
 
 @Component({
@@ -7,25 +8,44 @@ import { LetturaService } from '../common/lettura.service';
   styleUrls: ['./dati.component.css']
 })
 export class DatiComponent {
-  vettoreDati: any; 
+  vettoreDati: any[] = []; 
+  url: string; 
   stringa: string; 
+  altreRazze: boolean; 
+  numeroPg: number = 0; 
   datiPronti: boolean; 
 
   constructor(public leggi: LetturaService){
     this.stringa = ""; 
+    this.altreRazze = false; 
+    this.url = "https://api.api-ninjas.com/v1/dogs?name=dog";
     this.datiPronti = false; 
   }
 
   letturaDeiDati() {
     this.stringa = "In attesa dei dati";
-    this.leggi.getDati("https://api.api-ninjas.com/v1/dogs?name=a").subscribe(dati => {
+    this.leggi.getDati(this.url + "&offset=" + this.numeroPg +"0").subscribe(dati => {
       if (typeof dati[0] === 'string' || dati[0] instanceof String) { //dati non arrivati 
         this.stringa = "Si è verificato un errore";
       } else { //dati arrivati correttamente
         this.stringa = ""; 
         this.vettoreDati = dati;
+        if(this.vettoreDati.length == 20){
+          this.altreRazze = true; 
+        } else if(this.vettoreDati.length < 20){
+          this.altreRazze = false; 
+        }
         console.log(this.vettoreDati);
       }
     })
+  }
+
+  paginaSuccessiva(){
+    this.numeroPg += 2;
+    this.letturaDeiDati(); 
+  }
+  paginaPrecedente(){
+    this.numeroPg -= 2;
+    this.letturaDeiDati(); 
   }
 }
