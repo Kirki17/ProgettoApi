918c1774bacbb88550a52d697c075dd6708c8a56
Lucrezia Kircanski <lucrezia.kircanski@marconirovereto.it>
2023-05-21 18:34:39 +0200

    aggiunta filtri

--- razzeCanine/src/app/dati/dati.component.ts
+++ razzeCanine/src/app/dati/dati.component.ts
@@ -11,71 +11,75 @@ import { Dog } from '../dog';
 })
 export class DatiComponent {
   vettoreDati: Dog[] = [];
-  ricerca: string = ""; 
+  ricerca: string = "";
   altreRazze: boolean;
   numeroPg: number = 0;
   datiPronti: boolean;
-  urlBase: string; 
-  stringa: string; 
-  account: string; 
-  loginCheck : boolean; 
+  urlBase: string;
+  stringa: string;
+  account: string;
+  loginCheck: boolean;
+  filtri: boolean; 
 
-  ngOnInit(){
+  ngOnInit() {
     this.leggi.setPage('/');
   }
 
-  ngDoCheck(){
+  ngDoCheck() {
     console.log("Cambiamento");
     this.loginCheck = this.leggi.getLogin();
-    this.datiPronti = this.leggi.getDatiPronti(); 
-    if(this.loginCheck){ //true = loggato
-      this.account = "Loggato"; 
+    this.datiPronti = this.leggi.getDatiPronti();
+    if (this.loginCheck) { //true = loggato
+      this.account = "Loggato";
     } else {
-      this.account = "Login"; 
+      this.account = "Login";
     }
   }
 
   constructor(public leggi: LetturaService) {
-    this.loginCheck = leggi.getLogin(); 
+    this.loginCheck = leggi.getLogin();
     this.altreRazze = false;
     this.datiPronti = false;
-    leggi.setDatiPronti(this.datiPronti); 
-    if(this.loginCheck){ //true = loggato
-      this.account = "Loggato"; 
+    this.filtri = true; 
+    leggi.setDatiPronti(this.datiPronti);
+    if (this.loginCheck) { //true = loggato
+      this.account = "Loggato";
     } else {
-      this.account = "Login"; 
+      this.account = "Login";
     }
     //this.ricerca = "golden retriever";
-    this.stringa = ""; 
+    this.stringa = "";
     this.urlBase = "https://api.api-ninjas.com/v1/dogs?name=";
   }
 
   letturaDeiDati() {
     this.stringa = "In attesa dei dati";
-    this.vettoreDati = []; 
+    this.vettoreDati = [];
     this.leggi.getDati(this.urlBase + this.ricerca.trim() + "&offset=" + this.numeroPg + "0").subscribe(dati => {
-      if (typeof dati[0] === 'string' || dati[0] instanceof String || dati[0] == null) { //dati non arrivati 
+      if (typeof dati[0] === 'string' || dati[0] instanceof String) { //dati non arrivati 
         this.stringa = "Si è verificato un errore";
         this.vettoreDati = [];
         this.datiPronti = false;
-        this.leggi.setDatiPronti(this.datiPronti); 
-      } else { //dati arrivati correttamente
+        this.leggi.setDatiPronti(this.datiPronti);
+      } else if(dati[0] == null){
+        this.stringa = "Nessuna corrispondenza";
+      }else { //dati arrivati correttamente
         this.stringa = "";
-        let id: number = 0; 
-       dati.forEach((doggo:any) => {
-          this.vettoreDati.push(new Dog(doggo.barking, doggo.coat_length, doggo.drooling, doggo.energy, doggo.good_with_children, doggo.good_with_other_dogs, 
-            doggo.good_with_strangers, doggo.grooming, doggo.image_link, doggo.max_height_female, doggo.max_height_male, doggo.max_life_expectancy, 
-            doggo.max_weight_female, doggo.max_weight_male, doggo.min_height_female, doggo.min_height_male, doggo.min_life_expectancy, 
-            doggo.min_weight_female, doggo.min_weight_male, doggo.name, doggo.playfulness, doggo.protectiveness, doggo.shedding, doggo.trainability,)); 
+        let id: number = 0;
+        dati.forEach((doggo: any) => {
+          this.vettoreDati.push(new Dog(doggo.barking, doggo.coat_length, doggo.drooling, doggo.energy, doggo.good_with_children, doggo.good_with_other_dogs,
+            doggo.good_with_strangers, doggo.grooming, doggo.image_link, doggo.max_height_female, doggo.max_height_male, doggo.max_life_expectancy,
+            doggo.max_weight_female, doggo.max_weight_male, doggo.min_height_female, doggo.min_height_male, doggo.min_life_expectancy,
+            doggo.min_weight_female, doggo.min_weight_male, doggo.name, doggo.playfulness, doggo.protectiveness, doggo.shedding, doggo.trainability,));
         });
         //this.vettoreDati = dati;
-        if(this.vettoreDati.length < 20){
-          this.altreRazze = false; 
+        if (this.vettoreDati.length < 20) {
+          this.altreRazze = false;
         } else {
-          this.altreRazze = true; 
+          this.altreRazze = true;
         }
         this.datiPronti = true;
-        this.leggi.setDatiPronti(this.datiPronti); 
+        this.leggi.setDatiPronti(this.datiPronti);
 
         console.log(this.vettoreDati);
       }
@@ -85,35 +89,39 @@ export class DatiComponent {
   paginaSuccessiva() {
     this.numeroPg += 2;
     this.datiPronti = false;
-    this.leggi.setDatiPronti(this.datiPronti); 
+    this.leggi.setDatiPronti(this.datiPronti);
     this.letturaDeiDati();
   }
-  
+
   paginaPrecedente() {
     this.numeroPg -= 2;
     this.datiPronti = false;
-    this.leggi.setDatiPronti(this.datiPronti); 
+    this.leggi.setDatiPronti(this.datiPronti);
     this.letturaDeiDati();
   }
 
   info(razza: string) {
-    let trovato: boolean = false; 
-    
+    let trovato: boolean = false;
+
     for (let i = 0; i < this.vettoreDati.length || !trovato; i++) {
-      if(this.vettoreDati[i].name == razza){
-        this.leggi.setCurrentDog(this.vettoreDati[i]); 
-        trovato = true; 
+      if (this.vettoreDati[i].name == razza) {
+        this.leggi.setCurrentDog(this.vettoreDati[i]);
+        trovato = true;
       }
     }
 
     this.datiPronti = false;
-    this.leggi.setDatiPronti(this.datiPronti); 
-    this.vettoreDati =[]; 
+    this.leggi.setDatiPronti(this.datiPronti);
+    this.vettoreDati = [];
   }
 
-  login(){
-    this.datiPronti = false; 
-    this.leggi.setDatiPronti(this.datiPronti); 
+  login() {
+    this.datiPronti = false;
+    this.leggi.setDatiPronti(this.datiPronti);
     console.log(this.vettoreDati);
+  }
+
+  showFiltri(){
+    this.filtri = !this.filtri;
   }
 }
